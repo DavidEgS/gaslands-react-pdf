a21aab78e6b79b4ceb76b74012d24e53de1bcba0
David EgertonSmith <david.egertonsmith@gmail.com>
2021-09-30 16:34:55 +0100

    header component, googlefonts added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 
 import '../App.css';
+import Header from './Header/Header.js';
 
 function App() {
   return (
     <div className="App">
       hello Ben
+      < Header />
     </div>
   );
 }
---
+++ src/Components/VehicleObject.js
@@ -0,0 +1,113 @@
+const vehicles = {
+  car: {
+    weight: middleweight,
+    crew: 2,
+    halfHull: middleweight,
+    maxgear: middleweight,
+    handling: 3
+  },
+
+  performaceCar: {
+    weight: middleweight,
+    crew: 1,
+    halfHull: 4,
+    maxgear: 6,
+    handling: 4
+  },
+
+  truck: {
+    weight: middleweight,
+    crew: 3,
+    halfHull: 6,
+    maxgear: 4,
+    handling: 2
+  },
+
+  heavyTruck: {
+    weight: heavyweight,
+    crew: 4,
+    halfHull: 7,
+    maxgear: 3,
+    handling: 2
+  },
+
+  buggy: {
+    weight: lightweight,
+    crew: 2,
+    halfHull: 3,
+    maxgear: 6,
+    handling: 4
+  },
+
+  bus: {
+    weight: heavyweight,
+    crew: 8,
+    halfHull: 8,
+    maxgear: 3,
+    handling: 2
+  },
+
+  bikeWithSidecar: {
+    weight: lightweight,
+    crew: 2,
+    halfHull: 2,
+    maxgear: 6,
+    handling: 5
+  },
+
+  dragRacer: {
+    weight: lightweight,
+    crew: 1,
+    halfHull: 2,
+    maxgear: 6,
+    handling: 4
+  },
+
+  gyrocopter: {
+    weight: middleweight,
+    crew: 1,
+    halfHull: 2,
+    maxgear: 6,
+    handling: 4
+  },
+
+  iceCreamTruck: {
+    weight: heavyweight,
+    crew: 2,
+    halfHull: 5,
+    maxgear: 4,
+    handling: 2
+  },
+
+  ambulance: {
+    weight: middleweight,
+    crew: 3,
+    halfHull: 4,
+    maxgear: 4,
+    handling: 3
+  },
+
+  helicopter: {
+    weight: heavyweight,
+    crew: 3,
+    halfHull: 4,
+    maxgear: 4,
+    handling: 3
+  },
+
+  tank: {
+    weight: heavyweight,
+    crew: 3,
+    halfHull: 10,
+    maxgear: 3,
+    handling: 4
+  },
+
+  warRig: {
+    weight: heavyweight,
+    crew: 5,
+    halfHull: 13,
+    maxgear: 4,
+    handling: 2
+  }
+}
