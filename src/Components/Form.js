c0324cb798da751a1df6af6e78e55fcd0c031c4a
Ben McLaren <benmclaren@live.co.uk>
2021-09-30 16:22:53 +0100

    implemented react-bootstrap-form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const Form = () => {
+
+  const [vehicle, setVehicle] = useState("all vehicles");
+
+  const handleSelect = (e) => {
+    console.log(e);
+  }
+
   return (
-    <form>
-      <select name='dropdown'>
-        <option>Car</option>
-        <option>Truck</option>
-      </select>
-    </form>
+    <DropdownButton onSelect={handleSelect}>
+      <Dropdown.Item eventKey="car">Car</Dropdown.Item>
+      <Dropdown.Item eventKey="truck">Truck</Dropdown.Item>
+    </DropdownButton>
   )
 }
 
