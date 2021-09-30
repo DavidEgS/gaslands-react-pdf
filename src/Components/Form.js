fc4f3a09db92a7944f608a63eaff5bfc5efac085
Ben McLaren <benmclaren@live.co.uk>
2021-09-30 16:00:03 +0100

    form component made

---
+++ src/Components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <form>
+      <select name='dropdown'>
+        <option>Car</option>
+        <option>Truck</option>
+      </select>
+    </form>
+  )
+}
+
+export default Form
