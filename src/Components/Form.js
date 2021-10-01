bdf08eabbf62f2f9437f142e69990ea0b49573c2
Ben McLaren <benmclaren@live.co.uk>
2021-10-01 14:40:18 +0100

    creating iteraion for option tag

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -10,6 +10,22 @@ const Form = () => {
     setVehicle(e.target.value);
   }
 
+  // dynamically generate the option tag for each of the different vehicles
+
+
+  const keys = (Vehicles) => {
+    const keysList = Object.keys(Vehicles);
+    // console.log(keysList);
+    keysList.forEach((key) => {
+      // console.log(key)
+      const option = `<option value="#{key}">#{key}</option>`;
+      console.log(option);
+    })   
+    
+  }
+
+  keys(Vehicles);
+
   return (
     <form>
       <select value={vehicle} onChange={handleSelect}>
