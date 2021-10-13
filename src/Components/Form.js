99c530bb43eda3e7328951eb21025cc24b5e6f4b
david egerton-smith <david.egertonsmith@gmail.com>
2021-10-13 22:42:09 +0100

    form now updating state array

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,10 +3,14 @@ import { alignPropType } from 'react-bootstrap/esm/DropdownMenu';
 import VehicleObject from './VehicleObject';
 
 const Form = ({ vehicles, setVehicles }) => {
-
+  // console.log(vehicles)
   const handleSelect = (e) => {
     // console.log(e.target.value);
-    setVehicles(e.target.value);
+    let newVehicles = [...vehicles]
+    console.log(newVehicles)
+    newVehicles[e.target.id] = e.target.value
+    console.log(newVehicles)
+    setVehicles(newVehicles);
   }
 
   const keys = (VehicleObject) => {
@@ -17,7 +21,7 @@ const Form = ({ vehicles, setVehicles }) => {
       return (
        formSelect
       );
-    }) 
+    })
     return tag;
   }
 
@@ -39,40 +43,40 @@ const Form = ({ vehicles, setVehicles }) => {
   return (
     // <form>
     //   { selectors() }
-    // </form> 
-    
+    // </form>
+
     <div>
-    <select value={vehicles} onChange={handleSelect}>
+    <select value={vehicles} onChange={handleSelect} id='0'>
       { keys(VehicleObject) }
-    </select> 
+    </select>
 
-    <select value={vehicles} onChange={handleSelect}>
+    <select value={vehicles} onChange={handleSelect} id='1'>
      { keys(VehicleObject) }
-    </select> 
+    </select>
 
-    <select value={vehicles} onChange={handleSelect}>
+    <select value={vehicles} onChange={handleSelect} id='2'>
       { keys(VehicleObject) }
     </select>
 
-    <select value={vehicles} onChange={handleSelect}>
+    <select value={vehicles} onChange={handleSelect} id='3'>
     { keys(VehicleObject) }
     </select>
 
-    <select value={vehicles} onChange={handleSelect}>
+      <select value={vehicles} onChange={handleSelect} id='4'>
     { keys(VehicleObject) }
     </select>
 
-    <select value={vehicles} onChange={handleSelect}>
+      <select value={vehicles} onChange={handleSelect} id='5'>
     { keys(VehicleObject) }
     </select>
 
-    <select value={vehicles} onChange={handleSelect}>
+      <select value={vehicles} onChange={handleSelect} id='6'>
     { keys(VehicleObject) }
-    </select> 
+    </select>
 
-    <select value={vehicles} onChange={handleSelect}>
+    <select value={vehicles} onChange={handleSelect} id='7'>
     { keys(VehicleObject) }
-    </select> 
+    </select>
     </div>
 
   )
